963477c2a8e7dd078c5bc2cb022db445965e442e
Ryan Flynn <122565977+ryanflynn-dev@users.noreply.github.com>
2024-01-26 20:10:17 +0000

    fixing API bug

--- src/generator.js
+++ src/generator.js
@@ -1,57 +1,33 @@
-const ADJECTIVE_API_URL = "https://random-word-form.repl.co/random/adjective";
-const ANIMAL_API_URL = "https://random-word-form.repl.co/random/animal";
-const NOUN_API_URL = "https://random-word-form.repl.co/random/noun";
+const WORD_API_URL = "https://random-word-api.vercel.app/api?words=1";
 
 export class Generator {
-    constructor(people, auxVerbArray) {
-        this.people = people;
-        this.auxVerbArray = auxVerbArray;
-    }
-    async generate() {
-        const person = this.getPerson();
-        const auxVerb = this.getAuxVerb();
-        const adj = await this.getAdjective();
-        const nou = await this.getNoun();
-        const ani = await this.getAnimal();
+  constructor(people, auxVerbArray) {
+    this.people = people;
+    this.auxVerbArray = auxVerbArray;
+  }
+  async generate() {
+    const person = this.getPerson();
+    const auxVerb = this.getAuxVerb();
+    const word = await this.getWord();
 
-        return person + auxVerb + adj;
+    return person + auxVerb + word;
+  }
+  async getWord() {
+    try {
+      const response = await fetch(WORD_API_URL);
+      const data = await response.json();
+      return data[0];
+    } catch (error) {
+      console.error("No Jon", error);
+      return "Boob";
     }
-    async getAdjective() {
-        try {
-            const response = await fetch(ADJECTIVE_API_URL);
-            const data = await response.json();
-            return data[0];
-        } catch (error) {
-            console.error('No Jon', error);
-            return 'Boob'
-        }
-    }    
-    async getAnimal() {
-        try {
-            const response = await fetch(ANIMAL_API_URL);
-            const data = await response.json();
-            return data[0];
-        } catch (error) {
-            console.error('No Jon', error);
-            return 'Boob'
-        }
-    }    
-    async getNoun() {
-        try {
-            const response = await fetch(NOUN_API_URL);
-            const data = await response.json();
-            return data[0];
-        } catch (error) {
-            console.error('No Jon', error);
-            return 'Boob'
-        }
-    }
-    getAuxVerb() {
-        const random = Math.floor(Math.random() * this.auxVerbArray.length);
-        return this.auxVerbArray[random];
-    }
-    getPerson() {
-        const random = Math.floor(Math.random() * this.people.length);
-        return this.people[random];
-    }
+  }
+  getAuxVerb() {
+    const random = Math.floor(Math.random() * this.auxVerbArray.length);
+    return this.auxVerbArray[random];
+  }
+  getPerson() {
+    const random = Math.floor(Math.random() * this.people.length);
+    return this.people[random];
+  }
 }
